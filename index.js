83fd27b9935f66f7297dc2651dc823db726fa24d
khayanga <khayangavirgil@gmail.com>
2024-06-20 17:01:14 +0300

    created a server

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 // Create a server
 
-console.log("Hello world oout there!")
+const express = require('express');
+const app = express()
+
+app.listen(5000,()=>{
+    console.log("Server is running on port 5000! Hurray")
+})
+// Basic route
+app.get('/', (req, res) => {
+    res.send('Server running on port 5000 successfully!');
+  });
