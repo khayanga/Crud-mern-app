d29263c9729e968d29254001c17af998d74ed600
khayanga <khayangavirgil@gmail.com>
2024-06-24 16:51:27 +0300

    Connecting to MongoDb

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 // Create a server
 
+
 const express = require('express');
 const app = express()
+const dotenv = require('dotenv');
+const mongoose = require('mongoose')
+
+// Middleware to parse JSON
+app.use(express.json());
+dotenv.config(); // Load environment variables from .env file
+
+//Connecting to MongoDB
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log('Connected to the database'))
+  .catch(err => console.log("Connection failed"));
+  
 
 app.listen(5000,()=>{
     console.log("Server is running on port 5000! Hurray")
@@ -10,3 +23,4 @@ app.listen(5000,()=>{
 app.get('/', (req, res) => {
     res.send('Server running on port 5000 successfully!');
   });
+
